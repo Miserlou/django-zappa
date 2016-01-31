ddd8590d93b0cb6a86db4a9ec4aa3c26a502e4fb
Rich Jones <rich@anomos.info>
2016-01-31 22:39:30 +0200

    0.7.0 posts works yay

--- django_zappa/handler.py
+++ django_zappa/handler.py
@@ -16,6 +16,7 @@ from io import BytesIO
 from django.apps import apps
 from django.conf import settings
 from django.core.handlers.base import BaseHandler
+from django.core.handlers.wsgi import WSGIHandler
 from django.core.handlers.wsgi import ISO_8859_1, UTF_8, WSGIRequest
 from django.core.signals import (
     got_request_exception, request_finished, request_started,
@@ -24,49 +25,49 @@ from django.db import close_old_connections
 
 from zappa.wsgi import create_wsgi_request
 
-class LambdaHandler(BaseHandler):
-    """
-    A HTTP Handler that can be used for testing purposes. Uses the WSGI
-    interface to compose requests, but returns the raw HttpResponse object with
-    the originating WSGIRequest attached to its ``wsgi_request`` attribute.
-    """
-    def __init__(self, enforce_csrf_checks=True, *args, **kwargs):
-        self.enforce_csrf_checks = enforce_csrf_checks
-        super(LambdaHandler, self).__init__(*args, **kwargs)
+# class LambdaHandler(BaseHandler):
+#     """
+#     A HTTP Handler that can be used for testing purposes. Uses the WSGI
+#     interface to compose requests, but returns the raw HttpResponse object with
+#     the originating WSGIRequest attached to its ``wsgi_request`` attribute.
+#     """
+#     def __init__(self, enforce_csrf_checks=True, *args, **kwargs):
+#         self.enforce_csrf_checks = enforce_csrf_checks
+#         super(LambdaHandler, self).__init__(*args, **kwargs)
 
-    def __call__(self, environ):
-        # Set up middleware if needed. We couldn't do this earlier, because
-        # settings weren't available.
-        if self._request_middleware is None:
-            self.load_middleware()
+#     def __call__(self, environ):
+#         # Set up middleware if needed. We couldn't do this earlier, because
+#         # settings weren't available.
+#         if self._request_middleware is None:
+#             self.load_middleware()
 
-        request_started.disconnect(close_old_connections)
-        request_started.send(sender=self.__class__, environ=environ)
-        request_started.connect(close_old_connections)
-        request = WSGIRequest(environ)
-        # sneaky little hack so that we can easily get round
-        # CsrfViewMiddleware.  This makes life easier, and is probably
-        # required for backwards compatibility with external tests against
-        # admin views.
-        request._dont_enforce_csrf_checks = not self.enforce_csrf_checks
+#         request_started.disconnect(close_old_connections)
+#         request_started.send(sender=self.__class__, environ=environ)
+#         request_started.connect(close_old_connections)
+#         request = WSGIRequest(environ)
+#         # sneaky little hack so that we can easily get round
+#         # CsrfViewMiddleware.  This makes life easier, and is probably
+#         # required for backwards compatibility with external tests against
+#         # admin views.
+#         request._dont_enforce_csrf_checks = not self.enforce_csrf_checks
 
-        # Request goes through middleware.
-        response = self.get_response(request)
-        # Attach the originating request to the response so that it could be
-        # later retrieved.
-        response.wsgi_request = request
+#         # Request goes through middleware.
+#         response = self.get_response(request)
+#         # Attach the originating request to the response so that it could be
+#         # later retrieved.
+#         response.wsgi_request = request
 
-        # We're emulating a WSGI server; we must call the close method
-        # on completion.
-        if response.streaming:
-            response.streaming_content = closing_iterator_wrapper(
-                response.streaming_content, response.close)
-        else:
-            request_finished.disconnect(close_old_connections)
-            response.close()                    # will fire request_finished
-            request_finished.connect(close_old_connections)
+#         # We're emulating a WSGI server; we must call the close method
+#         # on completion.
+#         if response.streaming:
+#             response.streaming_content = closing_iterator_wrapper(
+#                 response.streaming_content, response.close)
+#         else:
+#             request_finished.disconnect(close_old_connections)
+#             response.close()                    # will fire request_finished
+#             request_finished.connect(close_old_connections)
 
-        return response
+#         return response
 
 def lambda_handler(event, context):    
 
@@ -74,12 +75,22 @@ def lambda_handler(event, context):
     import django
     django.setup()
 
+    from django.conf import settings
+
     # This is a normal HTTP request
     if event.get('method', None):
-        handler = LambdaHandler()
-        environ = create_wsgi_request(event)
-        response = handler(environ)
+
+        environ = create_wsgi_request(event, script_name=settings.SCRIPT_NAME)
+
+        # This doesn't matter.
+        def start(a, b):
+            return
+
+        handler = WSGIHandler()
+        response = handler(environ, start)
+
         returnme = {'Content': response.content}
+
         for item in response.items():
             returnme[item[0]] = item[1]
         returnme['Status'] = response.status_code
@@ -99,7 +110,7 @@ def lambda_handler(event, context):
             b64_content = base64.b64encode(content)
             raise Exception(b64_content)
         elif response.status_code in [301, 302]:
-            location = returnme['Location'].replace("http://zappa", "")
+            location = returnme['Location'].replace(settings.SCRIPT_NAME, "/")
             raise Exception(location)
         else:
             return returnme
@@ -115,7 +126,7 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     event = {
-        "body": {},
+        "body": "hello=there",
         "headers": {
             "Via": "1.1 e604e934e9195aaf3e36195adbcb3e18.cloudfront.net (CloudFront)",
             "Accept-Language": "en-US,en;q=0.5",
@@ -130,11 +141,12 @@ if __name__ == "__main__":
             "CloudFront-Is-Tablet-Viewer": "false",
             "X-Forwarded-Port": "443",
             "CloudFront-Is-Mobile-Viewer": "false",
-            "CloudFront-Is-Desktop-Viewer": "true"
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "Content-Type": "application/x-www-form-urlencoded"
         },
+        "method": "POST",
         "params": {
-            "a_path": "asdf1",
-            "b_path": "asdf2",
+            "a_path": "post_echo",
         },
         "command": "check",
         "query": { 
