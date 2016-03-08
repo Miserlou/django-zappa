4a0deb283d5fa13c5c0e0c8ab9b921d6c11f9706
Rich Jones <rich@anomos.info>
2016-03-08 10:09:30 +0100

    Add optional --zip argument to update command

--- django_zappa/management/commands/update.py
+++ django_zappa/management/commands/update.py
@@ -15,6 +15,10 @@ class Command(ZappaCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('environment', nargs='+', type=str)
+        parser.add_argument('--zip',
+            dest='zip',
+            default=None,
+            help='Use a supplied zip file')
 
     def handle(self, *args, **options):  # NoQA
         """
@@ -28,8 +32,12 @@ class Command(ZappaCommand):
         # Load your AWS credentials from ~/.aws/credentials
         self.zappa.load_credentials()
 
-        # Create the Lambda Zip
-        self.create_package()
+        # Create the Lambda Zip,
+        # or used the supplied zip file.
+        if not options['zip']:
+            self.create_package()
+        else:
+            self.zip_path = options['zip']
 
         # Upload it to S3
         self.zappa.upload_to_s3(self.zip_path, self.s3_bucket_name)
@@ -41,6 +49,10 @@ class Command(ZappaCommand):
 
         # Remove the uploaded zip from S3, because it is now registered..
         self.zappa.remove_from_s3(self.zip_path, self.s3_bucket_name)
+
+        # Finally, delete the local copy our zip package
+        if self.zappa_settings[self.api_stage].get('delete_zip', True) and not options['zip']:
+            os.remove(self.zip_path)
 
         print("Your updated Zappa deployment is live!")
 
